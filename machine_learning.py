d017e37d66f1f4ca522311812e5ddacc2a407b51
phuongnam2002 <phuongnamdpn2k2@gmail.com>
2024-08-26 16:02:36 +0700

    chore: upload file

--- machine_learning.py
+++ machine_learning.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
     model = RandomForestClassifier(n_estimators=10000, random_state=42)
-    model.fit(X_train, y_train)
+    model.fit(X_train, y_train)git 
 
     y_pred = model.predict(X_test)
 
