a44249f0896699f8b70b5320b5e152b32a8ae14e
Đặng Phương Nam <namdang@vinades.vn>
2024-08-27 20:06:30 +0700

    chore: upload file

--- machine_learning.py
+++ machine_learning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 
 from preprocess_data import (
     preprocessing,
@@ -10,10 +10,17 @@ from preprocess_data import (
 )
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/raw_data/cleaned_twitter_data.csv')
+    df_twitter = pd.read_csv('data/raw_data/cleaned_twitter_data.csv')
+    df_guardian = pd.read_csv('data/raw_data/cleaned_guardian_df.csv')
 
-    inputs = df['text'].values.tolist()
-    labels = df['sentiment'].values.tolist()
+    inputs_twitter = df_twitter['text'].values.tolist()
+    labels_twitter = df_twitter['sentiment'].values.tolist()
+
+    inputs_guardian = df_guardian['text'].values.tolist()
+    labels_guardian = df_guardian['sentiment'].values.tolist()
+
+    inputs = inputs_twitter + inputs_guardian
+    labels = labels_twitter + labels_guardian
 
     inputs = list(map(preprocessing, inputs))
 
@@ -23,10 +30,12 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
     model = RandomForestClassifier(n_estimators=10000, random_state=42)
-    model.fit(X_train, y_train)git 
+    model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
 
     print(f"Accuracy: {accuracy}")
+    print("\nClassification Report:\n", report)
